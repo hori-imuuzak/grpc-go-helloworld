cae31f796e22c5d9a2c56e70221020070648dde0
堀知海 <kazuumi.hori@spacemarket.co.jp>
2020-07-04 08:24:56 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module helloworld/grpc/examples
+
+go 1.11
+
+require (
+	github.com/golang/mock v1.1.1
+	github.com/golang/protobuf v1.4.2
+	github.com/grpc-ecosystem/grpc-gateway v1.14.6
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/genproto v0.0.0-20200626011028-ee7919e894b5
+	google.golang.org/grpc v1.30.0
+	google.golang.org/protobuf v1.25.0
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ helloworld/greeter_gateway/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+
+	gw "helloworld/grpc/examples/helloworld/proto"
+)
+
+const (
+	port = ":50052"
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	endpoint := fmt.Sprintf("localhost:50051")
+	err := gw.RegisterGreeterHandlerFromEndpoint(ctx, mux, endpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(port, mux)
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
